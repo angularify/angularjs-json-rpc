c041a622be5543f872e923a7bd9c67484482efb3
cfairweather <cfairweather@cfairweather.com>
2013-12-11 20:42:07 -0800

    Changed example to use $http.jsonrpc

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-function TestController ($scope, jsonRpc) {
-	//jsonRpc.setup({'url': 'some-url'});
-	//jsonRpc.request('getStatus', [{param: "param"}]);
+//All you need to do to declare the dependency on the angularjs-json-rpc module is add it to the array when you create a module.
+
+angular.module('test-module-jsonrpc', ['angularjs-json-rpc'])
+.controller('TestController', function ($scope, $http) {
+	scope.name = "cfairweather";
+	scope.team = [];
+	scope.refresh = function(){
+		//url, method, parameters, config
+	        $http.jsonrpc('url/to/jsonrpc/service', 'methodToCall', [param1, param2, etc])
+	        .success(function(data, status, headers, config){
+	        	if(data.result.success){
+				scope.team = data.result.team;
+			}else{
+				scope.team = ["Unable to retrieve team members"];
+			}
+			
+		}).error(function(data, status, headers, config){
+	
+		});
+	};
+	scope.refresh();
 }
