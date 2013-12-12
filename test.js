9ff310d6fe6131e3034b80afd2a45710c91b3122
cfairweather <cfairweather@cfairweather.com>
2013-12-11 20:48:50 -0800

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,8 @@ angular.module('test-module-jsonrpc', ['angularjs-json-rpc'])
 		//url, method, parameters, config
 	        $http.jsonrpc('url/to/jsonrpc/service', 'methodToCall', [param1, param2, etc])
 	        .success(function(data, status, headers, config){
-	        	if(data.result.success){
+	        	if(data.result.team){
+	        		//If service returns team as an array
 				scope.team = data.result.team;
 			}else{
 				scope.team = ["Unable to retrieve team members"];
@@ -19,4 +20,4 @@ angular.module('test-module-jsonrpc', ['angularjs-json-rpc'])
 		});
 	};
 	scope.refresh();
-}
+});
